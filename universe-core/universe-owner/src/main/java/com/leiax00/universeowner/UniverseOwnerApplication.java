6ebc22aa324fe906bb9db85ec1cd316598c79180
Leiax00 <leiax00@outlook.com>
2020-09-24 20:20:03 +0800

    Initial commit

---
+++ universe-core/universe-owner/src/main/java/com/leiax00/universeowner/UniverseOwnerApplication.java
@@ -0,0 +1,13 @@
+package com.leiax00.universeowner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniverseOwnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniverseOwnerApplication.class, args);
+    }
+
+}
---
+++ universe-core/universe-owner/src/test/java/com/leiax00/universeowner/UniverseOwnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leiax00.universeowner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniverseOwnerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
