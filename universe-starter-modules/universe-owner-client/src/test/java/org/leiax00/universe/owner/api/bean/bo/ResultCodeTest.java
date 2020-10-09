ca86574925c8389fb70d7c8cbeb669b368ef9faf
leiax00 <leiax00@outlook.com>
2020-10-09 11:20:30 +0800

    提交配置文件

---
+++ universe-starter-modules/universe-owner-client/src/test/java/org/leiax00/universe/owner/api/bean/bo/ResultCodeTest.java
@@ -0,0 +1,12 @@
+package org.leiax00.universe.owner.api.bean.bo;
+
+
+import org.leiax00.universe.common.bean.common.ResultCode;
+import org.junit.jupiter.api.Test;
+
+class ResultCodeTest {
+    @Test
+    public void testEnum() {
+        System.out.println(ResultCode.STATUS_SUCCESS.name());
+    }
+}
