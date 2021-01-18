f31c58bff66b541672d30c318cbc54eb83d47e3e
leiax00 <leiax00@outlook.com>
2021-01-18 16:17:38 +0800

    sidemenu样式排版

--- universe-view/src/layout/modules/SideMenu.vue
+++ universe-view/src/layout/modules/SideMenu.vue
@@ -4,12 +4,23 @@
       title="工具栏"
       :visible.sync="showSide"
       direction="ltr"
-      size="16%"
+      size="300px"
       :before-close="handleClose"
     >
       <div class="operate_group">
-        <el-button type="primary" icon="el-icon-edit" circle @click="handleOperate({name:'md-editor', params: {to: 'home'}})" />
-        <el-button type="primary" icon="el-icon-edit" circle />
+        <el-button class="nav-md-edit" icon="el-icon-edit" circle @click="handleOperate({name:'md-editor', params: {to: 'home'}})" />
+        <el-button icon="el-icon-edit" circle />
+        <el-button icon="el-icon-edit" circle />
+        <el-button icon="el-icon-edit" circle />
+        <el-button icon="el-icon-edit" circle />
+        <el-button icon="el-icon-edit" circle />
+        <el-button icon="el-icon-edit" circle />
+        <el-button icon="el-icon-edit" circle />
+        <el-button icon="el-icon-edit" circle />
+        <el-button icon="el-icon-edit" circle />
+        <el-button icon="el-icon-edit" circle />
+        <el-button icon="el-icon-edit" circle />
+        <el-button icon="el-icon-edit" circle />
       </div>
     </el-drawer>
   </div>
@@ -66,7 +77,29 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+  @import "~@/styles/variables.scss";
+  @import "~@/styles/mixin.scss";
   ::v-deep .el-drawer {
-    background-color: #d4c8ff;
+    background-color: $background-color-font;
+  }
+  .operate_group {
+    @include margin-2-common(0, 1rem);
+    ::v-deep & button {
+      @include margin-2-common($horizontal:0.5rem);
+      font-size: 24px;
+    }
+
+    /*::v-deep & button:nth-child(4n + 1) {*/
+    /*  margin-left: 0 !important;*/
+    /*}*/
+
+    .nav-md-edit {
+      color: #ff6589;
+      background-color: #d4c8ff;
+    }
+  }
+
+  ::v-deep .el-drawer__header span:focus, ::v-deep .el-drawer:focus {
+    outline: 0;
   }
 </style>
